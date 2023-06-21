56a6a84313e18371e14e5f78da2a5dd06af9e741
Alex Colin <acolin3196@gmail.com>
2023-06-21 18:40:18 -0500

    added auto increment

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,13 @@ CREATE DATABASE employees_db;
 
 -- department table
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
 -- role table
 CREATE TABLE employee_role (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10, 2) NOT NULL,
   department_id INT,
@@ -18,7 +18,7 @@ CREATE TABLE employee_role (
 
 -- employee table
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer')
+const fs = require('fs');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -18,3 +19,63 @@ const connection = mysql.createConnection({
     console.log('You are not connected to the database.')});
 
 
+    
+    const db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_info'
+    },
+        console.log('Connected to the employees database')
+    );
+   
+   
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Would you like to do?",
+            name: "primary options",
+            choices: ["Views all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee's role"]
+        },
+        {
+            type: "list",
+            message: "Choose the Department you would like to view",
+            name: "department options",
+            choices: ["Sales", "Engineering", "Finance", "Legal"]
+        },
+        {
+            type: "list",
+            message: "Choose the Role you would like to view",
+            name: "role options",
+            choices: ["Account Executive", "Sales Manager", "Software Engineer", "Lead Engineer", "Accountant", "Chief Revenue Officer", "Lawyer", "Chief of Counsel"]
+        },
+        {
+            type: "list",
+            message: "Which Employee would you like to view",
+            name: "employee options",
+            choices: [""]
+        },
+        {
+            type: "input",
+            message: "Please enter the name of the Department you would like to add",
+            name: "add department",
+        },
+        {
+            type: "input",
+            message: "Please enter the title of the role you would like to add",
+            name: "add role",
+        },
+       
+        {
+            type: "input",
+            message: "Please enter the name of the new employee",
+            name: "add role",
+        },
+       
+        {
+            type: "input",
+            message: "What employee would you like to update?",
+            name: "add role",
+        },
+        
+    ])   
